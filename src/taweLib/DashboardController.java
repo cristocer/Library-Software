9700975f845867ed01218e036baa5b7e110a58cc
bfarrington <farrington.ben@gmail.com>
2018-11-20 17:17:59 +0000

    Added admin controls. Not yet checking to see if admin is authenticated.

--- src/taweLib/DashboardController.java
+++ src/taweLib/DashboardController.java
@@ -3,23 +3,29 @@ package taweLib;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class DashboardController extends Controller{
+    LoginController loginController;
 
-    private LoginController loginController;
     @FXML
     private ListView<String> resourcesList;
     @FXML
     private Text user_id;
+    @FXML
+    private VBox adminVBox;
 
+
+    @FXML
+    void adminButton(){ adminHandling(); }
     @FXML
     void logoutButton(ActionEvent event) {
         logoutHandling();
     }
     @FXML
     void transactionHistoryButton(ActionEvent event) {
-        viewTransactions();
+        transactionHandling();
     }
 
     /**
@@ -50,15 +56,26 @@ public class DashboardController extends Controller{
     }
     private void logoutHandling(){
         user_id.setText("nobody");
+        disableAdmin();
         loginController.logout();
         loginHandling();
     }
-    private void viewTransactions(){
+    private void transactionHandling(){
         Window<TransactionController> transactionWindow = new Window<>("TransactionHistory.fxml",600,400,"Transaction History");
         transactionWindow.makeModal();
         transactionWindow.showAndWait();
     }
-
+    private void adminHandling(){
+        toggleAdmin();
+    }
+    private void toggleAdmin(){
+        adminVBox.setDisable(!adminVBox.isDisable());
+        adminVBox.setVisible(!adminVBox.isVisible());
+    }
+    private void disableAdmin(){
+        adminVBox.setDisable(true);
+        adminVBox.setVisible(false);
+    }
     @Override
     public void close(){
         System.exit(0);
