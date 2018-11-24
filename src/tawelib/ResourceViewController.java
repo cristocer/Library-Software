609407613a67b2519d46cce360fbcfd7ea2b064e
BFarrington <farrington.ben@gmail.com>
2018-11-24 15:56:50 +0000

    view resource done button working.

--- src/tawelib/ResourceViewController.java
+++ src/tawelib/ResourceViewController.java
@@ -5,17 +5,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
-public class ResourceViewController {
+public class ResourceViewController extends Controller{
     @FXML
     private ListView<String> resourceDetails;
     @FXML
-    private Button doneButton;
-    @FXML
     private ImageView resourceImage;
-
-
+    @FXML
+    void doneButton(){
+        doneHandling();
+    }
     public void initialize(){
+        // Purely for testing purposes.
         Image image = null;
         try {
             image = new Image("file:testpic.png");
@@ -27,4 +29,8 @@ public class ResourceViewController {
         resourceDetails.getItems().add("test");
     }
 
+
+    private void doneHandling(){
+        close();
+    }
 }
