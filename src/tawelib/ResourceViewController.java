d15d8cd1435ad0e4f1178ec91b1d96b77d51300e
BFarrington <farrington.ben@gmail.com>
2018-11-24 15:51:00 +0000

    Added image functionality to the resource view.

--- src/tawelib/ResourceViewController.java
+++ src/tawelib/ResourceViewController.java
@@ -3,6 +3,7 @@ package tawelib;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class ResourceViewController {
@@ -14,5 +15,16 @@ public class ResourceViewController {
     private ImageView resourceImage;
 
 
+    public void initialize(){
+        Image image = null;
+        try {
+            image = new Image("file:testpic.png");
+        }
+        catch (Exception e){
+            System.exit(-1);
+        }
+        resourceImage.setImage(image);
+        resourceDetails.getItems().add("test");
+    }
 
 }
